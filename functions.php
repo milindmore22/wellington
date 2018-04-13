1257f857a994f6fe91e42b4e8430d154982d6319
Thomas W <kontakt@themecoder.de>
2018-04-13 15:57:44 +0200

    added local fonts

--- functions.php
+++ functions.php
@@ -74,7 +74,7 @@ if ( ! function_exists( 'wellington_setup' ) ) :
 		add_theme_support( 'woocommerce' );
 
 		// Add extra theme styling to the visual editor.
-		add_editor_style( array( 'css/editor-style.css', wellington_google_fonts_url() ) );
+		add_editor_style( array( 'css/editor-style.css', get_template_directory_uri() . '/assets/css/custom-fonts.css' ) );
 
 		// Add Theme Support for Selective Refresh in Customizer.
 		add_theme_support( 'customize-selective-refresh-widgets' );
@@ -161,9 +161,6 @@ function wellington_scripts() {
 	// Passing Parameters to navigation.js.
 	wp_localize_script( 'wellington-jquery-navigation', 'wellington_menu_title', esc_html__( 'Navigation', 'wellington' ) );
 
-	// Register and Enqueue Google Fonts.
-	wp_enqueue_style( 'wellington-default-fonts', wellington_google_fonts_url(), array(), null );
-
 	// Register Comment Reply Script for Threaded Comments.
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -174,22 +171,15 @@ add_action( 'wp_enqueue_scripts', 'wellington_scripts' );
 
 
 /**
- * Retrieve Font URL to register default Google Fonts
+ * Enqueue custom fonts.
  */
-function wellington_google_fonts_url() {
+function wellington_custom_fonts() {
 
-	// Set default Fonts.
-	$font_families = array( 'Gudea:400,400italic,700,700italic', 'Magra:400,400italic,700,700italic' );
+	// Register and Enqueue Theme Fonts.
+	wp_enqueue_style( 'wellington-custom-fonts', get_template_directory_uri() . '/assets/css/custom-fonts.css', array(), '20180413' );
 
-	// Build Fonts URL.
-	$query_args = array(
-		'family' => urlencode( implode( '|', $font_families ) ),
-		'subset' => urlencode( 'latin,latin-ext' ),
-	);
-	$fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
-
-	return apply_filters( 'wellington_google_fonts_url', $fonts_url );
 }
+add_action( 'wp_enqueue_scripts', 'wellington_custom_fonts', 1 );
 
 
 /**
