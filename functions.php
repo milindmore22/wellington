7a46a76758193f43f7e7bb6163c974a95c10877d
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-08-12 14:26:29 +0200

    added support for secondary navigation menu

--- functions.php
+++ functions.php
@@ -190,7 +190,7 @@ function wellington_scripts() {
 	wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
 
 	// Register and enqueue navigation.js.
-	if ( has_nav_menu( 'primary' ) ) {
+	if ( has_nav_menu( 'primary' ) || has_nav_menu( 'secondary' ) ) {
 		wp_enqueue_script( 'wellington-navigation', get_theme_file_uri( '/assets/js/navigation.js' ), array( 'jquery' ), '20191114', true );
 		$wellington_l10n = array(
 			'expand'   => esc_html__( 'Expand child menu', 'wellington' ),
